2937c1c873ac07fe3180baa5498dfbfc86b7dbbb
kayky <kaykyzioti@gmail.com>
2025-01-28 15:51:08 -0300

    1.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link, useParams } from 'react-router-dom'; // Importando os componentes necessários
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importando o Router corretamente
 import Home from './pages/Home';
 import Error from './pages/Error';
 import SelectProfessional from './pages/customer/SelectProfessional';
@@ -8,25 +8,27 @@ import SelectService from './pages/customer/SelectService';
 import Confirm from './pages/customer/Confirm';
 import Conclusion from './pages/customer/Conclusion';
 import Cancel from './pages/customer/Cancel';
-import LoginAdm from './pages/adm/LoginAdm'
+import LoginAdm from './pages/adm/LoginAdm';
 
 function App() {
   return (
-    <div >
+    <div>
       <title>NovusBarber</title>
-      <Routes>
-        {/* Rota raiz que aceita o parâmetro id */}
-        <Route path="/:id" element={<Home />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/selectprofessional" element={<SelectProfessional />} />
-        <Route path="/selectdateandhour" element={<SelectDateAndHour />} />
-        <Route path="/selectservice" element={<SelectService />} />
-        <Route path="/confirm" element={<Confirm />} />
-        <Route path="/conclusion" element={<Conclusion />} />
-        <Route path="/cancel" element={<Cancel />} />
-        <Route path="/loginadm" element={<LoginAdm />} />
-      </Routes>
+      <Router basename="/novusbarber"> {/* Aqui está a modificação */}
+        <Routes>
+          <Route path="/:id" element={<Home />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="/selectprofessional" element={<SelectProfessional />} />
+          <Route path="/selectdateandhour" element={<SelectDateAndHour />} />
+          <Route path="/selectservice" element={<SelectService />} />
+          <Route path="/confirm" element={<Confirm />} />
+          <Route path="/conclusion" element={<Conclusion />} />
+          <Route path="/cancel" element={<Cancel />} />
+          <Route path="/loginadm" element={<LoginAdm />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 export default App;
