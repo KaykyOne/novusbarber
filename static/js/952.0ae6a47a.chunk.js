93695d47ada0d77dcd039995228aecc1eac50492
kayky <kaykyzioti@gmail.com>
2025-01-28 15:28:58 -0300

    Updates

---
+++ static/js/952.0ae6a47a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknovusbarber=self.webpackChunknovusbarber||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0ae6a47a.chunk.js.map
